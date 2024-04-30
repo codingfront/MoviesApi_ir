6727ee6f1e58d19e9e624b94204b47fddc2452eb
iammilaad <miladheydari.work@gmail.com>
2024-04-30 13:28:36 +0330

    fork me img updated, DS_Store added to gitignore

--- local/resources/views/sections/navigation.blade.php
+++ local/resources/views/sections/navigation.blade.php
@@ -1,8 +1,8 @@
 <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
     <div class="container">
 
-        <a href="https://github.com/abbas-oveissi/MoviesApi_ir"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png" alt="Fork me on GitHub"></a>
-
+       
+        <a href="https://github.com/codingfront/MoviesApi_ir"><img style="position: absolute; top: 0; left: 0; border: 0;" decoding="async" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_darkblue_121621.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" loading="lazy" data-recalc-dims="1"></a>
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header page-scroll">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
